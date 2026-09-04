86fda4fb42996f7306e13054afe60f236c18ec22
rams3y <83500613+rams3y@users.noreply.github.com>
2026-09-04 18:59:43 +0300

    Update numberService.ts

--- src/services/numberService.ts
+++ src/services/numberService.ts
@@ -1,8 +1,23 @@
-import { db } from '../firebase';
+import { initializeApp, getFirestore } from 'firebase/app';
+import { getFirestore as getFs } from 'firebase/firestore';
 import {
   collection, doc, getDocs, addDoc, updateDoc, deleteDoc,
   onSnapshot, query, orderBy, serverTimestamp, writeBatch
 } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA5pX0wt83GUnjDRFdZvFbvWiV06n5dTb8",
+  authDomain: "rams3y-9c830.firebaseapp.com",
+  projectId: "rams3y-9c830",
+  storageBucket: "rams3y-9c830.firebasestorage.app",
+  messagingSenderId: "1079276062395",
+  appId: "1:1079276062395:web:2ffc7aa405d99ffe5f4b9e",
+  measurementId: "G-18E9F7N2NE"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFs(app);
+
 import { LifecellNumber, ServiceResult } from '../types';
 
 const ADMIN_PASSWORD = 'R4m$ey#2026_xK9@vQ7!Wz';
